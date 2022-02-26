9415cdd81147f9233eaba08b89c57e2c22058c26
anand <anand_levaku@hotmail.com>
2022-02-26 06:36:13 -0500

    first commit

---
+++ server/resolvers.js
@@ -0,0 +1,9 @@
+const db = require('./db')
+const Query = {
+    jobs: () =>db.jobs.list()
+};
+
+const Job = {
+    company: (job) => db.companies.get(job.companyId)
+}
+module.exports = {Query, Job};
